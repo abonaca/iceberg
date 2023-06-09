4f1f55ce51f237b82a3e2be3190e650914317f79
Ana Bonaca <abonaca@gmail.com>
2023-06-08 21:18:55 -0700

    run full mocks

--- scripts/run_mocks.py
+++ scripts/run_mocks.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     if len(args)<4:
         args += [False]
     
-    mock_stream(hid=523889, graph=False, i0=0, f=0.3, lowmass=True, verbose=False, halo=True, nskip=int(args[1]), istart=int(args[2]), test=bool(args[3]))
+    mock_stream(hid=523889, graph=False, i0=0, f=1, lowmass=True, verbose=False, halo=True, nskip=int(args[1]), istart=int(args[2]), test=bool(args[3]))
     #evolve_bar_stars(mw_label=args[1], Nskip=int(args[2]), iskip=int(args[3]), test=bool(args[4]), m=1e10*u.Msun, Nrand=4000)
