bb03f18781602834a3fa3a95c6024e4b1632eec5
Ana Bonaca <abonaca@gmail.com>
2024-03-25 09:43:15 -0700

    input halo id for cluster runs

--- scripts/run_mocks.py
+++ scripts/run_mocks.py
@@ -5,8 +5,7 @@ if __name__ == '__main__':
     """Wrapper to create mock stellar streams"""
     
     args = sys.argv
-    if len(args)<5:
+    if len(args)<6:
         args += [False]
     
-    mock_stream(hid=523889, graph=False, i0=0, f=1, lowmass=True, verbose=False, halo=False, nskip=int(args[1]), istart=int(args[2]), remaining=int(args[3]), test=bool(args[4]))
-    #evolve_bar_stars(mw_label=args[1], Nskip=int(args[2]), iskip=int(args[3]), test=bool(args[4]), m=1e10*u.Msun, Nrand=4000)
+    mock_stream(hid=args[1], graph=False, i0=0, f=1, lowmass=True, verbose=False, halo=False, nskip=int(args[2]), istart=int(args[3]), remaining=int(args[4]), test=bool(args[5]))
