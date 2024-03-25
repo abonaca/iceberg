a73c7df1e632b7db6ade5ecc38c6baada48b9ac8
Ana Bonaca <abonaca@gmail.com>
2024-03-25 09:50:41 -0700

    input halo id as integer

--- scripts/run_mocks.py
+++ scripts/run_mocks.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     if len(args)<6:
         args += [False]
     
-    mock_stream(hid=args[1], graph=False, i0=0, f=-1, lowmass=True, verbose=False, halo=False, nskip=int(args[2]), istart=int(args[3]), remaining=int(args[4]), test=bool(args[5]))
+    mock_stream(hid=int(args[1]), graph=False, i0=0, f=-1, lowmass=True, verbose=False, halo=False, nskip=int(args[2]), istart=int(args[3]), remaining=int(args[4]), test=bool(args[5]))
