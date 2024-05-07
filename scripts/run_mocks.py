e446251af14767a7cd682b72e5eed14fd299014a
Ana Bonaca <abonaca@gmail.com>
2024-05-07 12:23:24 -0400

    read fstar as float

--- scripts/run_mocks.py
+++ scripts/run_mocks.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     if len(args)<7:
         args += [False]
     
-    mock_stream(hid=int(args[1]), graph=False, i0=0, f=args[2], lowmass=True, verbose=False, halo=False, nskip=int(args[3]), istart=int(args[4]), remaining=int(args[5]), test=bool(args[6]))
+    mock_stream(hid=int(args[1]), graph=False, i0=0, f=float(args[2]), lowmass=True, verbose=False, halo=False, nskip=int(args[3]), istart=int(args[4]), remaining=int(args[5]), test=bool(args[6]))
